bbb3bced6591f547587e64cf8d11f5300cba14b3
math-a3k <rodrigo@crowdbotics.com>
2022-05-09 20:14:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mobile_app_2_1",
+  url: "https://mobile_app_2_1.botics.co",
+  api: "https://mobile_app_2_1.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
